953204d056f7609ac8036d6b3f165ac708f1ef5f
Eduardo Pérez Córdova <eduardo860@MacBook-Air-de-Eduardo.local>
2025-09-19 18:36:24 -0600

    Clase 19 de Sept

--- backend/test_main.py
+++ backend/test_main.py
@@ -8,7 +8,7 @@ import pytest
 client = TestClient(app)
 mongo_client = mongomock.MongoClient()
 database = mongo_client["practica1"]
-collection_historial = database.historial
+fake_collection_historial = database.historial
 
 @pytest.mark.parametrize(
     "numeroA, numeroB, resultado",[
@@ -28,3 +28,32 @@ def test_sumar(monkeypatch, numeroA, numeroB, resultado):
     assert response.status_code == 200
     data = response.json()
     assert data == {"a": float(numeroA), "b": float(numeroB), "resultado": float(resultado)}
+
+
+
+
+assert fake_collection_historial.find()
+
+def test_historial(monkeypatch):
+    monkeypatch.setatrr(main,"collection_historial", fake_collection_historial)
+
+    response = client.get("/calculadora/historial")
+    assert response.status_code == 200
+
+    # Obtenemos todos los documentos que ya fueron insertados por los tests de /sum
+    expected_data = list(fake_collection_historial.find({}))
+
+    historial = []
+    for document in expected_data:
+        historial.apend({
+            "a":document["a"],
+            "b":document["b"],
+            "resultado":document["resultado"],
+            "date": document["date"].isoformat()
+        })
+
+    print(f"Debug: expected_data: {historial}")
+    print(f"Debug: response.json(): {response.json()}")
+
+
+    assert response.json() == {"historial":historial}
